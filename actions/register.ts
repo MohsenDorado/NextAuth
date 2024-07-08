8a07310a610a1b66181c929bdb879fa2dc945bfe
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-08 23:08:17 +0330

    Register semi done!

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,36 @@
 "use server";
-
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import { db } from "@/lib/db";
 
-export const  register= async (values:z.infer<typeof RegisterSchema>)=>
-    {
-        const validatedFields=RegisterSchema.safeParse(values);
-        if (!validatedFields.success) {
-            return{error:"Invalid fields!"}
-            
-        }else
-        return{success:"Email sent!"}
-        
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  //email not taken
+  const existingUser=await db.user.findUnique({
+    where:{
+        email,
     }
+  })
+  if (existingUser) {
+    return{error:"Email already existing my friend!"}
+    
+  }
+  await db.user.create({
+    data:{
+        name,
+        email,
+        password:hashedPassword,
+    }
+  })
+
+
+  //todo send verify email!!!
+
+  return { success: "Email sent!" };
+};

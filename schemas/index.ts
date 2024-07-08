63286e040c7af5902b366d186bbaf619e99e8e93
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-08 21:59:13 +0330

    Register ui Done

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema=z.object({
         message:"Password required"
     })
 });
+
+export const RegisterSchema=z.object({
+    email:z.string().email({
+        message:"Email required"
+    }),
+    password:z.string().min(6,{
+        message:"Minimum 6 characters required!"
+    }),
+    name:z.string().min(3,{
+        message:"Name is Required"
+    })
+});
